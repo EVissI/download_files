64bd807f42684093c43e228fce7b9a173ffdcfcc
viss <egor-berezin@bk.ru>
2025-07-25 02:16:09 +1000

    commit

--- bot/common/utils/i18n.py
+++ bot/common/utils/i18n.py
@@ -11,13 +11,13 @@ def create_translator_hub() -> TranslatorHub:
             FluentTranslator(
                 locale="ru",
                 translator=FluentBundle.from_files(
-                    locale="ru-RU", filenames=["locales/ru/LC_MESSAGES/txt.ftl"]
+                    locale="ru-RU", filenames=["bot/locales/ru/LC_MESSAGES/txt.ftl "]
                 ),
             ),
             FluentTranslator(
                 locale="en",
                 translator=FluentBundle.from_files(
-                    locale="en-US", filenames=["locales/en/LC_MESSAGES/txt.ftl"]
+                    locale="en-US", filenames=["bot/locales/en/LC_MESSAGES/txt.ftl "]
                 ),
             ),
         ],
