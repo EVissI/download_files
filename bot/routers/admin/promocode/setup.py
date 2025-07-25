b448a7ca941cde7fbb69e3179d0514cabda50d06
viss <egor-berezin@bk.ru>
2025-07-25 20:01:36 +1000

    commit

--- bot/routers/admin/promocode/setup.py
+++ bot/routers/admin/promocode/setup.py
@@ -8,12 +8,12 @@ from bot.common.kbds.markup.promo_code import PromoKeyboard
 
 promo_setup_router = Router()
 
-@promo_setup_router.message.filter(F.text == AdminKeyboard.get_kb_text()['promo'], StateFilter(GeneralStates.promo_view))
+@promo_setup_router.message(F.text == AdminKeyboard.get_kb_text()['promo'], StateFilter(GeneralStates.promo_view))
 async def handle_back(message: Message, state: FSMContext):
     await state.set_state(GeneralStates.promo_view)
     await message.answer(message.text, reply_markup=PromoKeyboard.build())
 
-@promo_setup_router.message.filter(F.text == PromoKeyboard.get_kb_text()['back'], StateFilter(GeneralStates.promo_view))
+@promo_setup_router.message(F.text == PromoKeyboard.get_kb_text()['back'], StateFilter(GeneralStates.promo_view))
 async def handle_back(message: Message, state: FSMContext):
     await state.set_state(GeneralStates.admin_panel)
     await message.answer(message.text, reply_markup=AdminKeyboard.build())
