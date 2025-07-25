22147c5adc5254b46cc0324d777084a1e6431ae0
viss <egor-berezin@bk.ru>
2025-07-26 03:57:30 +1000

    commit

--- bot/routers/setup.py
+++ bot/routers/setup.py
@@ -1,10 +1,13 @@
 ﻿from aiogram import Router
+from bot.common.middlewares.sub_middleware import SubscriptionMiddleware
 from bot.routers.start import start_router
 from bot.routers.stat import stat_router
 from bot.routers.autoanaliz import auto_analyze_router
 from bot.routers.profile import profile_router
 from bot.routers.admin.setup import admin_setup_router
 setup_router = Router()
+
+auto_analyze_router.message.middleware(SubscriptionMiddleware())
 
 setup_router.include_routers(
     start_router,
