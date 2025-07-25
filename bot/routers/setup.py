1b4d11b946e4a6a7b31cfc4cf24ae235d62d0709
viss <egor-berezin@bk.ru>
2025-07-25 19:57:03 +1000

    commit

--- bot/routers/setup.py
+++ bot/routers/setup.py
@@ -3,12 +3,13 @@ from bot.routers.start import start_router
 from bot.routers.stat import stat_router
 from bot.routers.autoanaliz import auto_analyze_router
 from bot.routers.profile import profile_router
-
+from bot.routers.admin.setup import admin_setup_router
 setup_router = Router()
 
 setup_router.include_routers(
     start_router,
     stat_router,
     auto_analyze_router,
-    profile_router
+    profile_router,
+    admin_setup_router
 )
