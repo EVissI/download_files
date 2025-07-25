544af6aadad3d05c547778ba38bfbad2e6c94faf
viss <egor-berezin@bk.ru>
2025-07-26 03:42:25 +1000

    commit

--- bot/routers/admin/promocode/setup.py
+++ bot/routers/admin/promocode/setup.py
@@ -6,7 +6,18 @@ from bot.common.general_states import GeneralStates
 from bot.common.kbds.markup.admin_panel import AdminKeyboard
 from bot.common.kbds.markup.promo_code import PromoKeyboard
 
+from bot.routers.admin.promocode.create_promo import promo_create_router
+from bot.routers.admin.promocode.view_promo import view_promo_router
+from bot.routers.admin.promocode.delete_promo import deactivate_promo_router
+
+
+
 promo_setup_router = Router()
+promo_setup_router.include_routers(
+    promo_create_router,
+    view_promo_router,
+    deactivate_promo_router,
+)
 
 @promo_setup_router.message(F.text == AdminKeyboard.get_kb_text()['promo'], StateFilter(GeneralStates.admin_panel))
 async def handle_back(message: Message, state: FSMContext):
