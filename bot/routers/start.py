1351e88e41dfeefff1361daa5a0c8b6a5c351929
viss <egor-berezin@bk.ru>
2025-07-26 23:41:58 +1000

    commit

--- bot/routers/start.py
+++ bot/routers/start.py
@@ -8,7 +8,7 @@ from bot.db.dao import UserDAO
 from bot.db.models import User
 from bot.db.schemas import SUser
 from bot.config import settings
-
+from bot.config import translator_hub
 from typing import TYPE_CHECKING
 from fluentogram import TranslatorRunner
 if TYPE_CHECKING:
@@ -17,16 +17,20 @@ if TYPE_CHECKING:
 start_router = Router()
 
 @start_router.message(CommandStart())
-async def start_command(message: Message, i18n:TranslatorRunner, session_with_commit: AsyncSession):
+async def start_command(message: Message, session_with_commit: AsyncSession):
     user_data = message.from_user
     user_id = user_data.id
     user_info:User = await UserDAO(session_with_commit).find_one_or_none_by_id(user_id)
     if user_info and user_data.id in settings.ROOT_ADMIN_IDS and user_info.role != User.Role.ADMIN.value:
         user_info.role = User.Role.ADMIN.value
         await UserDAO(session_with_commit).update(user_info.id, SUser.model_validate(user_info.to_dict()))
+        i18n: TranslatorRunner = translator_hub.get_translator_by_locale(
+        user_info.lang_code if user_info.lang_code else 'en'
+        )
         await message.answer(
             i18n.user.static.hello(), reply_markup=MainKeyboard.build(user_info.role, i18n)
         )
+        
         return
     if user_info is None:
         role = User.Role.USER.value
@@ -37,11 +41,18 @@ async def start_command(message: Message, i18n:TranslatorRunner, session_with_co
                             last_name=user_data.last_name, 
                             username=user_data.username,
                             role=role)
-        await UserDAO(session_with_commit).add(user_schema)
+
+        user_info = await UserDAO(session_with_commit).add(user_schema)
+        i18n: TranslatorRunner = translator_hub.get_translator_by_locale(
+        user_info.lang_code if user_info.lang_code else 'en'
+        )
         await message.answer(
-            i18n.user.static.hello(), reply_markup=MainKeyboard.build(user_info.role, i18n)
+            i18n.user.static.hello(),reply_markup=MainKeyboard.build(user_info.role, i18n)
         )
         return
+    i18n: TranslatorRunner = translator_hub.get_translator_by_locale(
+        user_info.lang_code if user_info.lang_code else 'en'
+        )
     await message.answer(
         i18n.user.static.hello(), reply_markup=MainKeyboard.build(user_info.role, i18n)
     )
