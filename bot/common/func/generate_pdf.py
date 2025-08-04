ec877e8639f42dbc223ecc80706dcff929d7734a
viss <egor-berezin@bk.ru>
2025-08-04 16:35:57 +1000

    commit

--- bot/common/func/generate_pdf.py
+++ bot/common/func/generate_pdf.py
@@ -1,8 +1,12 @@
 ﻿from weasyprint import HTML
 import io
 
+def convert_newlines_to_br(text: str) -> str:
+    """Преобразует символы новой строки \n в HTML-тег <br>."""
+    return text.replace("\n", "<br>")
+
 def html_to_pdf_bytes(html_text: str) -> bytes:
     pdf_io = io.BytesIO()
-    HTML(string=html_text).write_pdf(pdf_io)
+    HTML(string=convert_newlines_to_br(html_text)).write_pdf(pdf_io)
     pdf_io.seek(0)
     return pdf_io.read()
