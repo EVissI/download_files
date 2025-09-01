95674aaff2bbacf576eac4b02b73cfbfe6146a5d
viss <egor-berezin@bk.ru>
2025-09-02 06:04:05 +1000

    fix

--- bot/routers/autoanalize/batch.py
+++ bot/routers/autoanalize/batch.py
@@ -544,7 +544,7 @@ async def finalize_batch(
         }
         sorted_players = sorted(players_avg_pr.items(), key=lambda x: x[1])
         players_order_str = ",".join([player for player, _ in sorted_players])
-        players_order_str = players_order_str + f"_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pdf"
+        players_order_str = players_order_str + f"_({datetime.now().strftime('%d.%m.%y_%H:%M')}).pdf"
         await redis_client.set(
             f"pdf_file_name:{user_info.id}",
             players_order_str,
