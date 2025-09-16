67a53edb1f9ec1b0e1430a4477a6d6d62dcef97c
eviss <90708659+EVissI@users.noreply.github.com>
2025-09-17 00:01:17 +1000

    fix

--- bot/routers/admin/setup.py
+++ bot/routers/admin/setup.py
@@ -34,7 +34,8 @@ admin_setup_router.include_routers(
     payment_setup_router,
     broadcast_router,
     user_setting_router,
-    message_for_new_router
+    message_for_new_router,
+    user_group_router
 )
 
 @admin_setup_router.message(F.text.in_(get_all_locales_for_key(translator_hub, "keyboard-admin-reply-admin_panel")))
