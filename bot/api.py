675e15d5da383ae8944258ccc85a3718554c770b
eviss <90708659+EVissI@users.noreply.github.com>
2025-11-12 16:52:11 +1000

    fix

--- bot/api.py
+++ bot/api.py
@@ -2,8 +2,18 @@
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import Response
 
 from bot.routers.admin.hint_viewer_router import hint_viewer_api_router
+
+class NoCacheStaticFiles(StaticFiles):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def get_response(self, path: str, scope):
+        response = await super().get_response(path, scope)
+        response.headers["Cache-Control"] = "no-cache"
+        return response
 
 app = FastAPI(title="Backgammon Hint Viewer API", version="1.0.0")
 
@@ -16,7 +26,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="bot/static", cache_control="no-cache"), name="static")
+app.mount("/static", NoCacheStaticFiles(directory="bot/static"), name="static")
 templates = Jinja2Templates(directory="bot/templates")
 
 # Include routers
