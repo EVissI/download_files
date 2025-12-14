3cec9850f590581e2ad56e20047ee828866d0ec4
eviss <90708659+EVissI@users.noreply.github.com>
2025-12-14 13:33:26 +1000

    delete

--- front/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {GamesClient} from "@/components/Games";
-
-export default function Home() {
-    return (
-        <main className="min-h-screen">
-            <GamesClient/>
-        </main>
-    );
-}
