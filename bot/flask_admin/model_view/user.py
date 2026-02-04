2992162e5911ebfaac8b5c6ced11bbfe18cb5f76
eviss <90708659+EVissI@users.noreply.github.com>
2026-02-05 09:47:47 +1000

    fix

--- bot/flask_admin/model_view/user.py
+++ bot/flask_admin/model_view/user.py
@@ -2,7 +2,7 @@ from flask_appbuilder import ModelView
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder.models.decorators import renders
 from bot.db.models import User, UserPromocode, UserAnalizePayment, Promocode
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
 from sqlalchemy import func, select
 from flask import request
 
@@ -82,8 +82,14 @@ class UserModelView(ModelView):
     def get_query(self):
         """Переопределяем запрос для поддержки сортировки по промокодам"""
         query = super().get_query()
+        
+        # Всегда загружаем связанные данные для отображения промокодов и платежей
+        # Используем selectinload, который работает лучше с подзапросами
         query = query.options(
-            joinedload(User.used_promocodes).joinedload(UserPromocode.promocode)
+            selectinload(User.used_promocodes).selectinload(UserPromocode.promocode),
+            selectinload(User.used_promocodes).selectinload(UserPromocode.remaining_services),
+            selectinload(User.analize_payments_assoc).selectinload(UserAnalizePayment.analize_payment),
+            selectinload(User.analize_payments_assoc).selectinload(UserAnalizePayment.remaining_services),
         )
         
         # Проверяем параметры сортировки (flask_appbuilder использует формат _oc_ViewName и _od_ViewName)
@@ -116,5 +122,8 @@ class UserModelView(ModelView):
                 query = query.order_by(subquery.c.min_promo_code.asc().nullslast())
             else:
                 query = query.order_by(subquery.c.min_promo_code.desc().nullslast())
+            
+            # Используем distinct() чтобы избежать дублирования записей при outerjoin
+            query = query.distinct()
         
         return query
